9da85c55635f60e57a8924098a835b518c85112b
wis1011cen <wis1011cen@gmail.com>
2023-08-16 13:11:32 +0900

    手の

--- run_roi.py
+++ run_roi.py
@@ -14,13 +14,91 @@ import src.utils as utils
 # 14 - right elbow
 # 15 - left wrist
 # 16 - right wrist
+# 17 - left pinky
+# 18 - right pinky
+# 19 - left index
+# 20 - right index
+# 21 - left thumb
+# 22 - right thumb
+        
+def pose_detector_callback(result, output_frame, timestamp):
+    global annotated_frame
 
+    
+    
+    annotated_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
+    cropped_frame = np.copy(annotated_frame)
+    
+    for name, (x, y, w, h) in appliance_dict.items():
+        cv2.putText(annotated_frame, name, (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        cv2.rectangle(annotated_frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+    
+    height, width = annotated_frame.shape[:2]
+    landmark_names = ('l_shoulder', 'r_shoulder', 'l_elbow', 'r_elbow', 'l_wrist', 'r_wrist',
+                      'l_pinky', 'r_pinky', 'l_index', 'r_index', 'l_thumb', 'r_thumb')
+    landmark_dict = dict()
+    pose_landmarks_list = result.pose_landmarks
+    
+    for idx in range(len(pose_landmarks_list)):
+        pose_landmarks = pose_landmarks_list[idx]
+        for i , landmark_name in enumerate(landmark_names, 11):
+            landmark_cordinate = np.array([int(pose_landmarks[i].x * width), int(pose_landmarks[i].y * height)])
+            landmark_dict[landmark_name] = landmark_cordinate
+        # 肩のVisibility
+        landmark_dict['l_visibility'] = pose_landmarks[11].visibility
+        landmark_dict['r_visibility'] = pose_landmarks[12].visibility
+        
+        
+        #l_hand_center_x = int(pose_landmarks[19].x * width)
+        #l_hand_center_y = int(pose_landmarks[19].y * height)
+       # print(landmark_dict)
+        
+        #r_hand_center = pose_landmarks[20].x 
+        w1 = abs(landmark_dict['r_thumb'][0] - landmark_dict['r_pinky'][0])
+        w2 = abs(landmark_dict['r_index'][0] - landmark_dict['r_wrist'][0])
+        
+        w = max(w1, w2)
+
+        h1 = abs(landmark_dict['r_thumb'][1] - landmark_dict['r_pinky'][1])
+        h2 = abs(landmark_dict['r_index'][1] - landmark_dict['r_wrist'][1])
+        
+        h = max(h1, h2)
+        
+        min_x = min(landmark_dict['r_thumb'][0], landmark_dict['r_index'][0], landmark_dict['r_pinky'][0], landmark_dict['r_wrist'][0])
+        min_y = min(landmark_dict['r_thumb'][1], landmark_dict['r_index'][1], landmark_dict['r_pinky'][1], landmark_dict['r_wrist'][1])
+        max_x = max(landmark_dict['r_thumb'][0], landmark_dict['r_index'][0], landmark_dict['r_pinky'][0], landmark_dict['r_wrist'][0])
+        max_y = max(landmark_dict['r_thumb'][1], landmark_dict['r_index'][1], landmark_dict['r_pinky'][1], landmark_dict['r_wrist'][1])
+            
+        
+        #
+        #cropped_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
+        cv2.rectangle(annotated_frame, (min_x - 5*w, min_y - 5*h),(max_x + 5*w, max_y + 5*h), (0, 255, 0), 2)
+        r_cropped_frame = cropped_frame[min_y-5*h : max_y + 5*h, min_x - 5*w : max_x + 5*w]
+        print(r_cropped_frame.shape)
+        if r_cropped_frame.shape[0] > 0 and r_cropped_frame.shape[1] > 0:
+            mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(r_cropped_frame, cv2.COLOR_BGR2RGB))
+       
+            gesture_recognizer.recognize_async(mp_frame, timestamp)
+        
+        
+        
+        
+        
+        utils.arm_operation(landmark_dict, annotated_frame, appliance_dict, pre_gesture_dict)
+            
+        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        pose_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x,y=landmark.y, z=landmark.z) for landmark in pose_landmarks])
+        solutions.drawing_utils.draw_landmarks(annotated_frame,
+                                            pose_landmarks_proto,
+                                            solutions.pose.POSE_CONNECTIONS,
+                                            solutions.drawing_styles.get_default_pose_landmarks_style())
+    
+    
 def gesture_recognizer_callback(result, output_frame, timestamp):
-    global pre_gesture_dict, annotated_frame
+    global pre_gesture_dict
     #annotated_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
     gestures =  result.gestures
     handedness = result.handedness
-    
     
     if gestures:
         for hand, gesture in zip(handedness, gestures):
@@ -51,44 +129,6 @@ def gesture_recognizer_callback(result, output_frame, timestamp):
     #                               solutions.hands.HAND_CONNECTIONS,
     #                               solutions.drawing_styles.get_default_hand_landmarks_style(),
     #                               solutions.drawing_styles.get_default_hand_connections_style())
-
-
-                                                
-
-        
-def pose_detector_callback(result, output_frame, timestamp):
-    global annotated_frame
-
-    gesture_recognizer.recognize_async(output_frame, timestamp)
-    
-    annotated_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
-    
-    for name, (x, y, w, h) in appliance_dict.items():
-        cv2.putText(annotated_frame, name, (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-        cv2.rectangle(annotated_frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
-    
-    height, width = annotated_frame.shape[:2]
-    landmark_names = ('l_shoulder', 'r_shoulder', 'l_elbow', 'r_elbow', 'l_wrist', 'r_wrist')
-    landmark_dict = dict()
-    pose_landmarks_list = result.pose_landmarks
-    
-    for idx in range(len(pose_landmarks_list)):
-        pose_landmarks = pose_landmarks_list[idx]
-        for i , landmark_name in enumerate(landmark_names, 11):
-            landmark_cordinate = np.array([int(pose_landmarks[i].x * width), int(pose_landmarks[i].y * height)])
-            landmark_dict[landmark_name] = landmark_cordinate
-            landmark_dict['l_visibility'] = pose_landmarks[11].visibility
-            landmark_dict['r_visibility'] = pose_landmarks[12].visibility
-        
-        
-        utils.arm_operation(landmark_dict, annotated_frame, appliance_dict, pre_gesture_dict)
-            
-        pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        pose_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x,y=landmark.y, z=landmark.z) for landmark in pose_landmarks])
-        solutions.drawing_utils.draw_landmarks(annotated_frame,
-                                            pose_landmarks_proto,
-                                            solutions.pose.POSE_CONNECTIONS,
-                                            solutions.drawing_styles.get_default_pose_landmarks_style())
    
   
    
@@ -169,6 +209,7 @@ def main():
         mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         
         pose_detector.detect_async(mp_frame, timestamp)
+        #gesture_recognizer.recognize_async(mp_frame, timestamp)
         
         
         
