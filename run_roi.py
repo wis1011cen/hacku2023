00316f79cf4ddc9cd27ac68a8ec0bbaa65326df4
wis1011cen <wis1011cen@gmail.com>
2023-08-19 13:34:00 +0900

    first commit

--- run_roi.py
+++ run_roi.py
@@ -72,16 +72,22 @@ def pose_detector_callback(result, output_frame, timestamp):
     
     
 def gesture_recognizer_callback(result, output_frame, timestamp):
-    global gesture_dict
-    #annotated_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
+    global gesture_dict, annotated_frame, rotated_frame, cropped_frame
+
+    annotated_frame = np.copy(cv2.cvtColor(output_frame.numpy_view(), cv2.COLOR_RGB2BGR))
+    
+    for name, (x, y, w, h) in appliance_dict.items():
+        cv2.putText(annotated_frame, name, (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        cv2.rectangle(annotated_frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+        
     gestures =  result.gestures
     handedness = result.handedness
     # print(gestures)
     # print(handedness)
     # print()
     gesture_dict = {'Left': 'None', 'Right': 'None'}
+    
     if gestures:
-        
         for hand, gesture in zip(handedness, gestures):
             hand = hand[0].category_name
             gesture = gesture[0].category_name
@@ -95,6 +101,16 @@ def gesture_recognizer_callback(result, output_frame, timestamp):
                     gesture_dict['Left'] = gesture
         
     # print(timestamp, gesture_dict)
+    multi_hand_landmarks_list = [multi_hand_landmarks for multi_hand_landmarks in result.hand_landmarks]
+    for hand_landmarks in multi_hand_landmarks_list:
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks])
+
+        solutions.drawing_utils.draw_landmarks(annotated_frame,
+                                  hand_landmarks_proto,
+                                  solutions.hands.HAND_CONNECTIONS,
+                                  solutions.drawing_styles.get_default_hand_landmarks_style(),
+                                  solutions.drawing_styles.get_default_hand_connections_style())
                 
 
    
@@ -120,27 +136,33 @@ def main():
     print(f'resolution:{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}x{cap.get(cv2.CAP_PROP_FRAME_WIDTH)}')
     print('FPS:' ,cap.get(cv2.CAP_PROP_FPS))
    
-    pose_detector_options = vision.PoseLandmarkerOptions(base_options=python.BaseOptions(model_asset_path='models/pose_landmarker_lite.task'),
-                                                         running_mode=vision.RunningMode.LIVE_STREAM,
-                                                         result_callback=pose_detector_callback)
+    # pose_detector_options = vision.PoseLandmarkerOptions(base_options=python.BaseOptions(model_asset_path='models/pose_landmarker_lite.task'),
+    #                                                      running_mode=vision.RunningMode.LIVE_STREAM,
+    #                                                      result_callback=pose_detector_callback)
     
     CATEGORY_ALLOWLIST = ["None", "Closed_Fist", "Open_Palm", "Pointing_Up", "Thumb_Down", "Thumb_Up", "Victory", "ILoveYou"]
+    canned_gesture_classifier_options = mp.tasks.components.processors.ClassifierOptions(display_names_locale= None,
+    max_results = None,
+    score_threshold = None,
+    category_allowlist=CATEGORY_ALLOWLIST,
+    category_denylist = None)
     
+    #print(dataclasses.field(default_factory=_ClassifierOptions))
     gesture_recognizer_options = vision.GestureRecognizerOptions(base_options=python.BaseOptions(model_asset_path='models/gesture_recognizer.task'),
                                                                 running_mode=vision.RunningMode.LIVE_STREAM,
                                                                 num_hands=4,
-                                                                min_hand_detection_confidence = 0.5,
-                                                                min_hand_presence_confidence = 0.5,
+                                                                min_hand_detection_confidence = 0.3,
+                                                                min_hand_presence_confidence = 0.3,
                                                                 min_tracking_confidence= 0.5,
-                                                                canned_gesture_classifier_options=mp.tasks.components.processors.ClassifierOptions(category_allowlist = CATEGORY_ALLOWLIST),
+                                                                #canned_gesture_classifier_options=dataclasses.field(default_factory=_ClassifierOptions),#canned_gesture_classifier_options,
                                                                 #custom_gesture_classifier_options: mp.tasks.components.processors.ClassifierOptions = dataclasses.field(default_factory=_ClassifierOptions),
                                                                 result_callback=gesture_recognizer_callback)
-
+    print(gesture_recognizer_options)
     gesture_recognizer = vision.GestureRecognizer.create_from_options(gesture_recognizer_options)
 
     gesture_dict = {'Left': 'None', 'Right': 'None'}
 
-    pose_detector =  vision.PoseLandmarker.create_from_options(pose_detector_options)
+    # pose_detector =  vision.PoseLandmarker.create_from_options(pose_detector_options)
     
     appliance_dict = {'tv':(0, 200*SCALE, 100*SCALE, 160*SCALE), 'fan':(550*SCALE, 260*SCALE, 90*SCALE, 100*SCALE)}
     # l_start_time_dict = dict()
@@ -180,15 +202,15 @@ def main():
         
         mp_frame = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         
-        pose_detector.detect_async(mp_frame, timestamp)
-        #gesture_recognizer.recognize_async(mp_frame, timestamp)
+        # pose_detector.detect_async(mp_frame, timestamp)
+        gesture_recognizer.recognize_async(mp_frame, timestamp)
         
         
         
         #print('timestamp',timestamp)
         
         #print('FPS', fps)
-        #cv2.putText(annotated_frame, f'FPS:{fps:.1f}', (0, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(annotated_frame, f"{gesture_dict['Left']}", (0, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
         # for name, (x, y, w, h) in appliance_dict.items():
         #     cv2.putText(annotated_frame, name, (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
         #     cv2.rectangle(annotated_frame, (x,y),(x+w, y+h), (0,0,255), 2)
